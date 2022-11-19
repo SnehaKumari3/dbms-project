715de215dcb668668fefbdc45a023060b9b0c73d
sneha <sneha113kumari@gmail.com>
2022-11-19 09:48:51 +0530

    adding some comments

--- config.php
+++ config.php
@@ -1,8 +1,22 @@
 <?php
+    //declare server -> Localhost
     $host = "127.0.0.1";
-    $user = "root";                     
-    $pass = "";                                  
+    //declare user
+    $user = "root";      
+    //password
+    $pass ="";   
+    //name of database                               
     $db = "movietheatredb";
     $port = 3306;
-     $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+    //create connection
+    $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+    //checking connection established or not 
+    if(mysqli_connect_errno()){
+        echo "Failed to connect";
+        exit();
+    }
+
+    // else{
+    //     echo "Connection Successfully";
+    // }
 ?>
